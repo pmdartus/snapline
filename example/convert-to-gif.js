54e5479331d8af3d1875f8d5830e6b84fa49d6ee
Pierre-Marie Dartus <p.dartus@salesforce.com>
2016-03-07 07:35:48 +0100

    Try streams

--- example/convert-to-gif.js
+++ example/convert-to-gif.js
@@ -1,8 +1,22 @@
 'use strict'
 
-const snapline = require('..')
-const timeline = require('./loading.json')
+const fs = require('fs')
+const JSONStream = require('JSONStream')
+const _ = require('highland')
+const spawn = require('child_process').spawn
 
-snapline.toGif(timeline, { output: 'test.gif', fps: 10 })
-  .then(gifPath => console.log(`The gif(t) is ready: ${gifPath}!`))
-  .catch(console.error)
+// const convert = spawn('convert', ['gif:-', '-'])
+
+const readLoading = fs.createReadStream('./example/loading.json')
+const isScreenshotEntry = data => data.name === 'Screenshot'
+
+_(readLoading).through(JSONStream.parse('*'))
+  .filter(isScreenshotEntry)
+  .each(data => console.log(data.name))
+  // .pipe(es.mapSync(data => new Buffer(data.args.snapshot, 'base64')))
+  // .pipe()
+
+// let i = 0
+// s.on('end', () => console.log('end'))
+// s.on('data', data => console.log(i++))
+// s.on('error', err => console.log(err))
